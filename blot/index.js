b87b37d7d706310774356f3718fcc134b5e4dca8
Briyan George Dyju <70208092+briyandyju09@users.noreply.github.com>
2024-07-27 16:01:30 +0400

    Update index.js

--- blot/index.js
+++ blot/index.js
@@ -1,19 +1,27 @@
 function drawScaledPattern() {
-  const numPoints = bt.randIntInRange(1, 200);
+ const numPoints = bt.randIntInRange(1, 200);
+ const radius = 30;
+ const rotationSpeed = 2;
 
-  for (let i = 0; i < numPoints; i++) {
-    const angle = (i * 2 * Math.PI) / numPoints;
-    const x = 55 + 30 * Math.cos(angle);
-    const y = 80 + 30 * Math.sin(angle);
+ let polylines = [];
 
-    const star = [];
-    for (let j = 0; j < 10; j++) {
-      const angle2 = (j * 2 * Math.PI) / 10;
-      star.push([x + 12 * Math.cos(angle2), y + 12 * Math.sin(angle2)]);
-    }
+ for (let i = 0; i < numPoints; i++) {
+  const angle = (i * 2 * Math.PI) / numPoints;
+  const x = 55 + radius * Math.cos(angle);
+  const y = 80 + radius * Math.sin(angle);
 
-    drawLines(bt.rotate([star], 2, [60, 60]));
-  }
+  let star = [];
+  for (let j = 0; j < 10; j++) {
+   const angle2 = (j * 2 * Math.PI) / 10;
+   const x2 = x + 12 * Math.cos(angle2);
+   const y2 = y + 12 * Math.sin(angle2);
+   star.push([x2, y2]);
+  }
+  polylines.push(star);
+ }
+
+ polylines = bt.rotate(polylines, rotationSpeed, [60, 60]);
+ drawLines(polylines);
 }
 
 drawScaledPattern();
